3e9d79164b35ac47cac2bef24d2bf8ec1a13c68b
Tathya Dixit <dixit.tathya2003@gmail.com>
2026-01-31 12:53:06 +0530

    fix : minor fixes

--- tasks/models.py
+++ tasks/models.py
@@ -3,10 +3,10 @@ from django.contrib.auth.models import User
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=30, blank=False)
+    name = models.CharField(max_length=30)
     description = models.TextField(blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='categories')
-    created_at = models.DateField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name 
@@ -24,14 +24,15 @@ class Task(models.Model):
         DONE = 'DO', 'Done'
         ARCHIVED = 'AR', 'Archived'
 
-    title = models.CharField(max_length=30, blank=False)
+    title = models.CharField(max_length=30)
     description = models.TextField(blank=True)
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tasks')
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, related_name='tasks')
     priority = models.IntegerField(choices=Priority.choices, default=Priority.NORMAL)
     status = models.CharField(max_length=2, choices=Status.choices, default=Status.PENDING)
-    due_date = models.DateField(blank=True)
-    created_at = models.DateField(auto_now_add=True)
-    done_date = models.DateField(blank=True)
+    due_date = models.DateTimeField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    done_date = models.DateTimeField(blank=True, null=True)
 
     def __str__(self):
         return f"{self.title} - {self.status}"
